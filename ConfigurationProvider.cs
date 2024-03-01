d3ff82f20046109dba6494083d777812e075b2a1
venkatK585 <161845565+venkatK585@users.noreply.github.com>
2024-03-01 13:29:55 +0000

    VenkatKura
    NHG Test

---
+++ ConfigurationProvider.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TechnicalTest
+{
+   public class ConfigurationProvider
+    {
+        private readonly IConfiguration _config;
+
+        public ConfigurationProvider()
+        {
+            _config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile($"appsettings.json", false)
+                .Build();
+        }
+
+        public string GetUrl()
+        {
+            return _config.GetSection("url").Value;
+        }
+    }
+}
---
+++ LocationClient.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.Extensions.Configuration;
+using NUnit.Framework;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using TechnicalTest.Service;
+
+namespace TechnicalTest
+{
+    public class LocationClient
+    {
+        private readonly RestService _resrService;
+        private readonly ConfigurationProvider _configurationProvider;
+        private RestResponse Response;
+
+        public LocationClient(RestService restService, ConfigurationProvider configurationProvider)
+        {
+            _resrService = restService;
+            _configurationProvider = configurationProvider;
+        }
+
+        public void GetLocationInformation(string countryCode, string postCode)
+        {
+            var url = _configurationProvider.GetUrl();
+           
+        }
+
+        public void VerifyRequestStatus(string isSuccessful)
+        {
+            if (isSuccessful == "true")
+            {
+                Assert.That(Response.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+            }
+            else
+            {
+                Assert.That(Response.StatusCode, Is.EqualTo(HttpStatusCode.NotFound));
+            }
+
+        }
+    }
+}
+
